120efedad8aa5951e454df954c1cae29fabcb9e5
unknown <agnor99@users.noreply.github.com>
2020-12-02 21:46:19 +0100

    Day1 Part 2

--- src/com/agnor99/solver/first/Solver.java
+++ src/com/agnor99/solver/first/Solver.java
@@ -11,6 +11,7 @@ public class Solver extends AbstractSolver {
     public void solve() {
         values = getInteger("first");
         System.out.println(getSolution());
+        System.out.println(getSolutionForThree());
     }
     private int getSolution() {
         for(int i = 0; i < values.size()-1; i++) {
@@ -22,4 +23,18 @@ public class Solver extends AbstractSolver {
         }
         return 0;
     }
+
+    private int getSolutionForThree() {
+        for(int i = 0; i < values.size()-2; i++) {
+            for(int j = i+1; j < values.size() -1; j++) {
+                for(int k = j+1; k < values.size(); k++) {
+                    if (values.get(i) + values.get(j) + values.get(k)== 2020) {
+                        return values.get(i) * values.get(j) * values.get(k);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
 }
